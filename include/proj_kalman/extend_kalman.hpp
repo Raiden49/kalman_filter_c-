2894ebd59e80316028e2e0fccbd7e7c15273fdc3
Raiden <138971294+Raiden49@users.noreply.github.com>
2023-10-19 23:06:54 +0800

    Update extend_kalman.hpp

--- include/proj_kalman/extend_kalman.hpp
+++ include/proj_kalman/extend_kalman.hpp
@@ -10,53 +10,53 @@ const double DEC = M_PI / 180;
 class ExtendKalman : public KalmanInterface {
     public:
 
-		/**
-		 * @brief Construction of Extend Kalman filter class
-		 * 
-		 * @param dim_x dimension of state(x_k)
-		 * @param dim_z dimension of measurement(z_k)
-		 * @param dim_u dimension of control(u_k)
-		 */
+	/**
+	* @brief Construction of Extend Kalman filter class
+	* 
+	* @param dim_x dimension of state(x_k)
+	* @param dim_z dimension of measurement(z_k)
+	* @param dim_u dimension of control(u_k)
+	*/
         ExtendKalman(int dim_x, int dim_z, int dim_u) 
             : KalmanInterface(dim_x, dim_z, dim_u) {
             H = Eigen::MatrixXd::Zero(dim_z, dim_x);
         }
 
         /**
-		 * @brief Construction of Extend Kalman filter interface class
-		 * 
-		 * @param dim_x dimension of state(x_k)
-		 * @param dim_z dimension of measurement(z_k)
-		 * @param dim_u dimension of control(u_k)
-         * @param Q process error covariance matrix
-         * @param R measurement error covariance matrix
-		 */
+	* @brief Construction of Extend Kalman filter interface class
+	* 
+	* @param dim_x dimension of state(x_k)
+	* @param dim_z dimension of measurement(z_k)
+	* @param dim_u dimension of control(u_k)
+        * @param Q process error covariance matrix
+        * @param R measurement error covariance matrix
+	*/
         ExtendKalman(int dim_x, int dim_z, int dim_u, const Eigen::MatrixXd Q, 
             const Eigen::MatrixXd R) : ExtendKalman(dim_x, dim_z, dim_u) {
             this->Q = Q;
             this->R = R;
         }
 
-	    /**
-		* @brief initialization of extend kalman filter(override)
-		*
-		* @param x_k initialization of state
-		*/
+	/**
+	* @brief initialization of extend kalman filter(override)
+	*
+	* @param x_k initialization of state
+	*/
         void init(Eigen::VectorXd &x_k) override;
 
         /**
-		* @brief prediction of extend kalman filter(override)
-		*
-		* @param u controlling quantity
+	* @brief prediction of extend kalman filter(override)
+	*
+	* @param u controlling quantity
         * @param t step size
-		*/
+	*/
         Eigen::VectorXd predict(Eigen::VectorXd &u, double t) override;
 
         /**
-		* @brief update of extend kalman filter(override)
-		*
-		* @param z_k measurement quantity
-		*/      
+	* @brief update of extend kalman filter(override)
+	*
+	* @param z_k measurement quantity
+	*/      
         Eigen::VectorXd update(Eigen::VectorXd &z_k) override;
 
         // The following functions are for reference only
