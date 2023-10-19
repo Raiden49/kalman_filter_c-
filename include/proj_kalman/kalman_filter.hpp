9c8e0f4e13925317046d879476279f06cc4c78ec
Raiden <138971294+Raiden49@users.noreply.github.com>
2023-10-19 23:07:39 +0800

    Update kalman_filter.hpp

--- include/proj_kalman/kalman_filter.hpp
+++ include/proj_kalman/kalman_filter.hpp
@@ -7,31 +7,31 @@ namespace proj_kalman {
 class KalmanFilter : public KalmanInterface {
     public:
 
-		/**
-		 * @brief Construction of Kalman filter class
-		 * 
-		 * @param dim_x dimension of state(x_k)
-		 * @param dim_z dimension of measurement(z_k)
-		 * @param dim_u dimension of control(u_k)
-		 */
+	/**
+	* @brief Construction of Kalman filter class
+	* 
+	* @param dim_x dimension of state(x_k)
+	* @param dim_z dimension of measurement(z_k)
+	* @param dim_u dimension of control(u_k)
+	*/
         KalmanFilter(int dim_x, int dim_z, int dim_u) 
             : KalmanInterface(dim_x, dim_z, dim_u) {
             if (dim_u > 0) 
                 B = Eigen::MatrixXd::Zero(dim_x, dim_u);
             else
                 B = Eigen::MatrixXd::Zero(dim_x, dim_x);
-            H = Eigen::MatrixXd::Zero(dim_z, dim_x);
+            	H = Eigen::MatrixXd::Zero(dim_z, dim_x);
         }
 
         /**
-		 * @brief Construction of Kalman filter interface class
-		 * 
-		 * @param dim_x dimension of state(x_k)
-		 * @param dim_z dimension of measurement(z_k)
-		 * @param dim_u dimension of control(u_k)
-         * @param Q process error covariance matrix
-         * @param R measurement error covariance matrix
-		 */
+	* @brief Construction of Kalman filter interface class
+	* 
+	* @param dim_x dimension of state(x_k)
+	* @param dim_z dimension of measurement(z_k)
+	* @param dim_u dimension of control(u_k)
+        * @param Q process error covariance matrix
+        * @param R measurement error covariance matrix
+	*/
         KalmanFilter(int dim_x, int dim_z, int dim_u, const Eigen::MatrixXd Q, 
             const Eigen::MatrixXd R, const Eigen::MatrixXd B) : KalmanFilter(dim_x, dim_z, dim_u) {
             this->Q = Q;
@@ -39,26 +39,26 @@ class KalmanFilter : public KalmanInterface {
             this->B = B;
         }
 
-	    /**
-		* @brief initialization of kalman filter(override)
-		*
-		* @param x_k initialization of state
-		*/
+	/**
+	* @brief initialization of kalman filter(override)
+	*
+	* @param x_k initialization of state
+	*/
         void init(Eigen::VectorXd &x_k) override;
 
         /**
-		* @brief prediction of kalman filter(override)
-		*
-		* @param u controlling quantity
+	* @brief prediction of kalman filter(override)
+	*
+	* @param u controlling quantity
         * @param t step size
-		*/
+	*/
         Eigen::VectorXd predict(Eigen::VectorXd &u, double t) override;
 
         /**
-		* @brief update of kalman filter(override)
-		*
-		* @param z_k measurement quantity
-		*/      
+	* @brief update of kalman filter(override)
+	*
+	* @param z_k measurement quantity
+	*/      
         Eigen::VectorXd update(Eigen::VectorXd &z_k) override;
 
     public:
